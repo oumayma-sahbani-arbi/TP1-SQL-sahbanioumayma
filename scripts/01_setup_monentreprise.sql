60fbd714a68148f754628ba5ad909ab34d68449b
unknown <you@example.comoumaymasahbani1994@gmail.com>
2026-02-24 10:46:20 +0100

    Initial commit- TP1 SQL

---
+++ scripts/01_setup_monentreprise.sql
@@ -0,0 +1,10 @@
+use [MonEntreprise]
+GO
+
+SELECT *
+FROM [MonEntreprise].[dbo].[Vente];
+
+SELECT[venteID]
+FROM [MonEntreprise].[dbo].[vente];
+
+SELECT DISTINCT [ClientID] FROM [MonEntreprise].[dbo].[Vente];
